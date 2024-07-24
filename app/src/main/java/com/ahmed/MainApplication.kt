f85699293bb5bfdbfb9a1677e486d8eb5760e54b
Ahmed Youssef <qahmed89@gmail.com>
2024-07-24 05:05:04 +0300

    Add Analytics with properties add MVI to home screen add change Token inside App Center

--- app/src/main/java/com/ahmed/MainApplication.kt
+++ app/src/main/java/com/ahmed/MainApplication.kt
@@ -8,6 +8,6 @@ import com.microsoft.appcenter.crashes.Crashes
 class MainApplication:Application() {
     override fun onCreate() {
         super.onCreate()
-        AppCenter.start(this,"6a8ea143-ec51-4183-8651-e9a04efe87c3",Analytics::class.java,Crashes::class.java)
+        AppCenter.start(this,"b3029dbb-ff3e-4fc2-8d95-f035dd219d9b",Analytics::class.java,Crashes::class.java)
     }
 }
--- app/src/main/java/com/ahmed/retirmentcalculator/screen/home/HomeViewModel.kt
+++ app/src/main/java/com/ahmed/retirmentcalculator/screen/home/HomeViewModel.kt
@@ -1,24 +1,43 @@
 package com.ahmed.retirmentcalculator.screen.home
 
 import androidx.lifecycle.ViewModel
+import com.microsoft.appcenter.analytics.Analytics
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import kotlin.math.pow
 
 class HomeViewModel : ViewModel() {
 
-   private val _state = MutableStateFlow(HomeStateUi())
+    private val _state = MutableStateFlow(HomeStateUi())
     val state = _state.asStateFlow()
 
-    fun onIntent(intent: HomeIntent){
-        when(intent){
-            HomeIntent.ClickOnCalculate -> TODO()
+    fun onIntent(intent: HomeIntent) {
+        when (intent) {
+            HomeIntent.ClickOnCalculate -> {
+                val proprieties = mapOf(
+                    "insertRate" to state.value.insertRate,
+                    "monthlySaving" to state.value.monthlySaving,
+                    "currentAge" to state.value.currentAge,
+                    "plannedRetirementAge" to state.value.plannedRetirementAge,
+                    "currentSaving" to state.value.currentSaving,
+                )
+                if (state.value.insertRate.toInt() <= 0) {
+                    Analytics.trackEvent("Wrong InsertRate", proprieties)
+                }
+                if (state.value.currentAge.toInt() <= 0) {
+                    Analytics.trackEvent("Wrong Current Age", proprieties)
+                }
+            }
+
             is HomeIntent.TypingInCurrentAge -> _state.update { it.copy(currentAge = intent.value) }
             is HomeIntent.TypingInCurrentSaving -> _state.update { it.copy(currentSaving = intent.value) }
             is HomeIntent.TypingInInsertRate -> _state.update { it.copy(insertRate = intent.value) }
             is HomeIntent.TypingInMonthlySaving -> _state.update { it.copy(monthlySaving = intent.value) }
-            is HomeIntent.TypingInPlannedRetirementAge -> _state.update { it.copy(plannedRetirementAge = intent.value) }
+            is HomeIntent.TypingInPlannedRetirementAge -> _state.update {
+                it.copy(
+                    plannedRetirementAge = intent.value
+                )
+            }
         }
     }
 
